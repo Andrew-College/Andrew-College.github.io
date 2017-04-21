066ee9a8965073b44c4d99a56d06265b2c388002
Andrew College <andrewsansomwork@gmail.com>
2017-04-21 01:33:59 +0100

    Making hidden content

--- js/lessons.js
+++ js/lessons.js
@@ -19,3 +19,21 @@ function lesson_clicked(elem) {
       'Ooh \'eck, I\'ve lost me notes!!! I can\'t find ' + lesson + ' anywhere!');
   }, 1000);
 }
+
+function show_hidden_content(elem, target_id)
+{
+  var hidden = $(elem).attr('class');
+
+  if (hidden === 'hidden-content')
+  {
+    $(target_id).removeClass('hidden');
+    $(elem).removeClass('hidden-content');
+    $(elem).addClass('shown-content');
+  }
+  else
+  {
+    $(target_id).addClass('hidden');
+    $(elem).removeClass('shown-content');
+    $(elem).addClass('hidden-content');
+  }
+}
